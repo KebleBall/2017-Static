a33f659e1952b825a1096defeb6dff7e2be52e83
tzakian <timothy.zakian@cs.ox.ac.uk>
2016-11-16 14:52:58 +0000

    Work?

---
+++ js/functions.js
@@ -0,0 +1,50 @@
+$(window).on("load", function() {
+
+  if ($(window).width() <= 1420) {
+    $('.ui.sidebar').sidebar('attach events', '.toc.item');
+  }
+
+
+  $(".se-pre-con").slideUp("slow"); //fadeOut("slow");
+  $('.ui.embed').embed();
+
+  setTimeout(function() {
+    $('#primaryText').show().addClass('animated fadeInUp');
+    //$('#date').show().addClass('animated fadeInUp');
+  }, 2500);
+
+  setTimeout(function() {
+    $('#date').show().addClass('animated fadeInRight');
+  }, 4500);
+
+  $(window).scroll(function() {
+    $('#date').addClass('animated fadeOut');
+    $('#secondaryText').addClass('animated fadeOut');
+  })
+
+
+  $('#interstitial').waypoint({
+    handler: function(direction) {
+      //$('#interstitial').find('h1').show().addClass('animated fadeInRight');
+      $('#interstitial').find('h1').show().addClass('animated fadeInDown');
+      // Only trigger it once
+      this.destroy();
+    },
+    offset: '85%'
+  });
+
+  $('#theTheory').waypoint({
+    handler: function(direction) {
+      if (direction == 'down') {
+        //$('#navBar').removeClass('no-show').addClass('animated fadeInDown');
+        $('#navBar').removeClass('no-show');
+      } else {
+        //$('#navBar').removeClass('animated fadeInDown').addClass('no-show');
+        $('#navBar').addClass('no-show');
+      }
+      console.log("Passed masthead: " + direction);
+    },
+    offset: 75
+  });
+
+});
---
+++ js/navBarClicker.js
@@ -0,0 +1,68 @@
+$(window).on("load", function() {
+
+  var makeActive = function(str) {
+    $('.menu').find('.active').removeClass('active');
+    $('.menu').find('a[href="' + str + '"]').addClass('active');
+  }
+
+  var navSections = ["'#'", "#food", "#entertainment", "#employment", "#sponsorship", "#committee"];
+
+  $('#primaryText').waypoint({
+    handler: function(direction) {
+      if (direction == 'up') {
+        makeActive('#');
+      }
+    },
+    offset: 66
+  });
+
+  $('#theTheory').waypoint({
+    handler: function(direction) {
+      makeActive('#theTheory');
+    },
+    offset: '25%'
+  });
+
+  $('#food').waypoint({
+    handler: function(direction) {
+      makeActive('#food');
+    },
+    offset: '25%'
+  });
+
+  $('#entertainment').waypoint({
+    handler: function(direction) {
+      makeActive('#entertainment');
+    },
+    offset: '25%'
+  });
+
+  $('#employment').waypoint({
+    handler: function(direction) {
+      makeActive('#employment');
+    },
+    offset: '25%'
+  });
+
+  $('#sponsorship').waypoint({
+    handler: function(direction) {
+      makeActive('#sponsorship');
+    },
+    offset: '25%'
+  });
+
+  $('#committee').waypoint({
+    handler: function(direction) {
+      makeActive('#committee');
+    },
+    offset: '25%'
+  });
+
+$('#keble2016').waypoint({
+  handler: function(direction) {
+    makeActive('#keble2016');
+  },
+  offset: '25%'
+});
+
+});
